c871e14cbe389bc49a440eb1e3f5894d6c36aebe
irfan-fadhlurrahman <irfanfadh43@gmail.com>
2023-04-03 14:30:03 +0700

    revise Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,40 +14,16 @@ provider "google" {
   credentials = file(var.credentials)
 }
 
-resource "google_compute_address" "dtc-de-ip" {
-  name = "dtc-de-ip"
-}
-
-module "dtc-de-network" {
-  source       = "./network"
-  network_name = "dtc-de-network"
-  target_tags  = ["vm-dtc-de"]
-}
-
-module "vm-dtc-de" {
-  source        = "./instance"
-  disk_name     = "dtc-de-disk"
-  disk_image    = "ubuntu-os-cloud/ubuntu-2204-jammy-v20230114"
-  zone          = var.zone
-  disk_type     = "pd-balanced"
-  disk_size_gb  = 50
-  instance_name = "vm-dtc-de"
-  instance_type = "c2d-standard-4"
-  tags          = ["vm-dtc-de"]
-  network_name  = "dtc-de-network"
-  ip_address    = google_compute_address.dtc-de-ip.address
-}
-
 module "data-lake-storage" {
   source          = "./storage"
-  bucket_name     = "${var.project_id}_${var.dataset_name}"
+  bucket_name     = "${var.project_id}_data_lake"
   bucket_location = var.dataset_location
   storage_class   = "STANDARD"
 }
 
 module "data-warehouse-bigquery" {
   source             = "./warehouse"
-  dataset_id         = var.dataset_name
+  dataset_id         = "data_warehouse"
   project_id         = var.project_id
   warehouse_location = var.dataset_location
 }
